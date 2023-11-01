ac6d7035b85b8591731b3ad6a938e3e39b7cfa16
devsylva <sylvaejike@gmail.com>
2023-11-01 11:10:24 +0100

    auth_app views

---
+++ auth_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import *
+
+router = DefaultRouter()
+
+urlpatterns = router.urls + [
+    path("signup/", signUp, name="signup"),
+    path("login/", MyObtainTokenPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", LogoutView.as_view(), name='logout'),
+    path('change-password/', change_password, name='change_password'),
+    path('update-user/', UserUpdateView.as_view(), name='update-user'),
+]
